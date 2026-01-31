a5e6a3684d7f3a4cfb6180597522876c6cf40aec
pedro batista <pedro2006batista@gmail.com>
2026-01-31 13:05:49 -0300

    Adicionar arquivos de projeto.

---
+++ Fundamentos/Comentarios.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    internal class Comentarios
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("ola");
+            // comentario
+        }
+    }
+}
---
+++ Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace CursoCSharp.Fundamentos
+{
+    internal class PrimeiroPrograma
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("Hello, World!");
+           
+        }
+
+    }
+}
