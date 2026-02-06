4bcc87c312f392a68ca09604432b423099cd5189
pedro batista <pedro2006batista@gmail.com>
2026-02-05 21:52:09 -0300

    interpolacoes de string

---
+++ Fundamentos/interpolacao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		public class Interpolacao
+		{
+			public static void Executar()
+			{
+				string nome = "Gustavo";
+				int idade = 23;
+				Console.WriteLine("Olá, {0}! Você tem {1} anos.", nome, idade); // usando o formato tradicional de interpolação
+				Console.WriteLine($"Olá, {nome}! Você tem {idade} anos."); // usando a interpolação de string (string interpolation) do C# 6.0 em diante
+
+        }
+    }
+}
