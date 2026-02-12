b0b8114511e5c2f04a51c64029a29cf74f43b96f
pedro batista <pedro2006batista@gmail.com>
2026-02-11 23:11:01 -0300

    operador atribuicao

---
+++ Fundamentos/OperadoresUnarios.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    internal class OperadoresUnarios
+    {
+        public static void Executar()
+        {
+            var valorNegativo = -5;
+            var valorPositivo = +valorNegativo;
+            Console.WriteLine(valorPositivo);
+            valorPositivo = -valorNegativo;
+            Console.WriteLine(valorPositivo);
+            int numero = 5;
+            Console.WriteLine("Incremento: " + ++numero);
+            Console.WriteLine("Decremento: " + --numero);
+        }
+    }
+}
