215ebc5198b871d5dacef1c9797125033dc15cd3
pedro batista <pedro2006batista@gmail.com>
2026-02-21 15:10:36 -0300

    Estrutura While

---
+++ EstruturaDeControle/EstruturaSwitch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.EstruturaDeControle
+{
+    internal class EstruturaSwitch
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("Avalie o meu atendimento de 1 a 5: ");
+            int.TryParse(Console.ReadLine(), out int nota);
+
+            switch (nota)
+            {
+                case 0:
+                    Console.WriteLine("Pessimo");
+                    break;
+                case 1:
+                case 2:
+                    Console.WriteLine("Ruim");
+                    break;
+                case 3:
+                    Console.WriteLine("Regular");
+                    break;
+                case 4:
+                    Console.WriteLine("Bom");
+                    break;
+                case 5:
+                    Console.WriteLine("Otimo");
+                    break;
+                default:
+                    Console.WriteLine("nota invalida");
+                    break;
+            }
+
+            Console.WriteLine("Obrigado por responder!");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -27,6 +27,7 @@ namespace CursoCSharp {
                 {"Estrutura If - Estrutura De Controle", EstruturaIf.Executar },
                 {"Estrutura If Else - Estrutura De Controle", EstruturaIfElse.Executar },
                 {"Estrutura If Else If - Estrutura De Controle", EstruturaIfElseIf.Executar },
+                {"Estrutura Switch - Estrutura De Controle", EstruturaSwitch.Executar },
 
 
             });
