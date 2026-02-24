569d55c36270ae27ac4172418d492231a510da5a
pedro batista <pedro2006batista@gmail.com>
2026-02-23 23:07:05 -0300

    Exercicios de cadastros de alunos

---
+++ Exercicios/ExercicioCadastrarAluno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Exercicios
+{
+    internal class ExercicioCadastrarAluno
+    {
+        public static void Executar() 
+        {
+            Console.WriteLine("Cadastro alunos");
+        
+        }
+    }
+}
